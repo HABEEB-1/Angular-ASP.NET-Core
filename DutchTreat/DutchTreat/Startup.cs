63401a46371f668da09577ef601739cace5ab53d
HABEEB-1 <mdhabeeburrahman706@gmail.com>
2023-03-06 02:53:16 +0530

    ASP.NET Core Project Basic Html

---
+++ DutchTreat/DutchTreat/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using IHostingEnvironment = Microsoft.Extensions.Hosting.IHostingEnvironment;
+
+namespace DutchTreat
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
